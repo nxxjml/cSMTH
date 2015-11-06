0cd333f889e24980cf18d7f5ac1c7c8c4545c32e
nxxjml <nxxjml@sina.cn>
2015-11-06 00:19:59 -0800

    project init

---
+++ cSMTH/cSMTH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cSMTH
+//
+//  Created by simao on 15/11/5.
+//  Copyright © 2015年 simao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cSMTH/cSMTH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cSMTH
+//
+//  Created by simao on 15/11/5.
+//  Copyright © 2015年 simao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
